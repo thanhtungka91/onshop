da07e216157b3eeec77d6d98e501fc2a2396ae6a
Vo Thanh Tung <thanhtungka91@gmail.com>
2016-12-16 00:54:14 +0900

    fk

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -29,7 +29,7 @@ attachment_config = {
     s3_headers:     { "Cache-Control" => "max-age=31557600" },
     s3_protocol:    "https",
     bucket:         "elasticbeanstalk-us-west-2-797738718062",
-    url:            "s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-797738718062/",
+    url:            "s3-us-west-2.amazonaws.com/",
 
     styles: {
         mini:     "48x48>",
